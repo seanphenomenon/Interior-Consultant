59fd68c7d0650b6b7f936cbb7cf47a2bc29d8fe0
seanphenomenon <seanvharrell@gmail.com>
2021-01-01 15:44:28 -0800

    edited mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-navBarToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("active");
-});
+  //Toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1.5
+        }s`;
+      }
+    });
+
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  });
+}
+
+navSlide();
